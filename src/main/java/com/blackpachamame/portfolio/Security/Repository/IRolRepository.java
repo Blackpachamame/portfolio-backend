6c2e92abc6ea09d8e3a816eae0a7685b5c9773d7
Marcos Travaglini <marcos.travaglini@est.fi.uncoma.edu.ar>
2023-03-03 00:35:06 -0300

    login con jwt parte 1

---
+++ src/main/java/com/blackpachamame/portfolio/Security/Repository/IRolRepository.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.blackpachamame.portfolio.Security.Repository;
+
+import com.blackpachamame.portfolio.Security.Entity.Rol;
+import com.blackpachamame.portfolio.Security.Enums.RolNombre;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author Travaglini
+ */
+@Repository
+public interface IRolRepository extends JpaRepository<Rol, Integer> {
+    Optional<Rol> findByRolNombre(RolNombre rolNombre);
+}
